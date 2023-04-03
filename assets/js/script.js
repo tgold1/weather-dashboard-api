a9f4bb8283285aa921d6d5a55caf3e849e3c0694
Tim Gold <timothygold@comcast.net>
2023-04-03 16:07:44 -0400

    added search function to link with getWeather in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,62 @@
 var APIKey = "1a6e242c584145cebf5c8827e5e6e268";
-var latitude = "39.9551221447199";
-var longitude = "-75.16952831929365";
-var searchURL = "https://api.openweathermap.org/data/2.5/forecast?lat=39.9551221447199&lon=-75.16952831929365&appid=1a6e242c584145cebf5c8827e5e6e268&units=imperial";
-var cityLocation; 
-var searchCity = JSON.parse(localStorage.getItem("cityView"));
-console.log(searchCity)
-
-var Searchbutton = document.querySelector(".Search");
-var forecastView = document.querySelector(".forecastView");
+var searchCity = localStorage.getItem("city");
+var searchButton = document.querySelector("#user-form");
+var forecastView = document.querySelector("#forecast");
 function getWeather(data) {
-var cityLocation = document.querySelector("#cityLocation");
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid=1a6e242c584145cebf5c8827e5e6e268&units=imperial";
+fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var cityLocation = document.querySelector("#cityLocation");
 var Temperature = document.querySelector("#Temperature");
 var Windspeed = document.querySelector("#Windspeed");
 var Humidity = document.querySelector("#Humidity") ; 
-  console.log(data);
-  cityLocation.textContent= data.city.name;
+  cityLocation.textContent= data.city.name + " (" + dayjs().format('M/D/YY') + ")";
   Temperature.textContent="Temperature: " + data.list[0].main.temp + " degrees";
   Windspeed.textContent="Windspeed: " + data.list[0].wind.speed + " mph";
   Humidity.textContent="Humidity: " + data.list[0].main.humidity + "%";
   var Boxone = document.querySelector("#boxone");
-  Boxone.innerHTML="Temperature: " + data.list[1].main.temp + " degrees<br/>Windspeed: " + data.list[1].wind.speed + " mph<br/>Humidity: " + data.list[1].main.humidity + "%";
+  Boxone.innerHTML="Temperature: " + data.list[8].main.temp + " degrees<br/>Windspeed: " + data.list[8].wind.speed + " mph<br/>Humidity: " + data.list[8].main.humidity + "%";
   var Boxtwo = document.querySelector("#boxtwo");
-  Boxtwo.innerHTML="Temperature: " + data.list[2].main.temp + " degrees<br/>Windspeed: " + data.list[2].wind.speed + " mph<br/>Humidity: " + data.list[2].main.humidity + "%";
+  Boxtwo.innerHTML="Temperature: " + data.list[16].main.temp + " degrees<br/>Windspeed: " + data.list[16].wind.speed + " mph<br/>Humidity: " + data.list[16].main.humidity + "%";
   var Boxthree = document.querySelector("#boxthree");
-  Boxthree.innerHTML="Temperature: " + data.list[3].main.temp + " degrees<br/>Windspeed: " + data.list[3].wind.speed + " mph<br/>Humidity: " + data.list[3].main.humidity + "%";
+  Boxthree.innerHTML="Temperature: " + data.list[24].main.temp + " degrees<br/>Windspeed: " + data.list[24].wind.speed + " mph<br/>Humidity: " + data.list[24].main.humidity + "%";
   var Boxfour = document.querySelector("#boxfour");
-  Boxfour.innerHTML="Temperature: " + data.list[4].main.temp + " degrees<br/>Windspeed: " + data.list[4].wind.speed + " mph<br/>Humidity: " + data.list[4].main.humidity + "%";
+  Boxfour.innerHTML="Temperature: " + data.list[32].main.temp + " degrees<br/>Windspeed: " + data.list[32].wind.speed + " mph<br/>Humidity: " + data.list[32].main.humidity + "%";
   var Boxfive = document.querySelector("#boxfive");
-  Boxfive.innerHTML="Temperature: " + data.list[5].main.temp + " degrees<br/>Windspeed: " + data.list[5].wind.speed + " mph<br/>Humidity: " + data.list[5].main.humidity + "%";
-}
-function getAPI(queryURL) {
+  Boxfive.innerHTML="Temperature: " + data.list[39].main.temp + " degrees<br/>Windspeed: " + data.list[39].wind.speed + " mph<br/>Humidity: " + data.list[39].main.humidity + "%";
+
+      localStorage.setItem("cityView", JSON.stringify(data));
+     
+    });
+  }
+function getAPI(city) {
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 fetch(queryURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       getWeather(data);
-      localStorage.setItem("cityView", JSON.stringify(data));
+      localStorage.setItem("city", city);
      
     });
-    Searchbutton.addEventListener('click' , function (event) {
-      console.log(forecastView.value)
-      getAPI(forecastView.value)
     
-    })
    
     
     
      
   }
 
- 
+  searchButton.addEventListener('submit' , function (event) {
+    event.preventDefault()
+    console.log(forecastView.value)
+    getAPI(forecastView.value)
+  
+  })
  
 //document.getElementById("latitude").value =
  // position.coords.latitude.toFixed(2);
@@ -68,4 +74,4 @@ function displayTime () {
 displayTime ();
 setInterval(displayTime, 1000);
 
-getAPI(searchURL); 
+getAPI(searchCity); 
