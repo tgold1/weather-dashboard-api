6224830a57c3ae6cf84379fe8adb8509567af527
Tim Gold <timothygold@comcast.net>
2023-04-03 17:21:13 -0400

    added weather icon images to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,20 +14,20 @@ fetch(queryURL)
 var Temperature = document.querySelector("#Temperature");
 var Windspeed = document.querySelector("#Windspeed");
 var Humidity = document.querySelector("#Humidity") ; 
-  cityLocation.textContent= data.city.name + " (" + dayjs().format('M/D/YY') + ")";
-  Temperature.textContent="Temperature: " + data.list[0].main.temp + " degrees";
-  Windspeed.textContent="Windspeed: " + data.list[0].wind.speed + " mph";
-  Humidity.textContent="Humidity: " + data.list[0].main.humidity + "%";
+  cityLocation.innerHTML= data.city.name + " (" + dayjs().format('M/D/YY') + ") <img src='https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png'/>";
+  Temperature.innerHTML="Temp: " + data.list[0].main.temp + "°F";
+  Windspeed.innerHTML="Wind: " + data.list[0].wind.speed + " mph";
+  Humidity.innerHTML="Humidity: " + data.list[0].main.humidity + "%";
   var Boxone = document.querySelector("#boxone");
-  Boxone.innerHTML="Temperature: " + data.list[8].main.temp + " degrees<br/>Windspeed: " + data.list[8].wind.speed + " mph<br/>Humidity: " + data.list[8].main.humidity + "%";
+  Boxone.innerHTML=dayjs().add(1, "day").format('M/D/YY') + "<br/><img src='https://openweathermap.org/img/wn/" + data.list[8].weather[0].icon + "@2x.png'/><br/>Temp: " + data.list[8].main.temp + "°F<br/>Wind: " + data.list[8].wind.speed + " mph<br/>Humidity: " + data.list[8].main.humidity + "%";
   var Boxtwo = document.querySelector("#boxtwo");
-  Boxtwo.innerHTML="Temperature: " + data.list[16].main.temp + " degrees<br/>Windspeed: " + data.list[16].wind.speed + " mph<br/>Humidity: " + data.list[16].main.humidity + "%";
+  Boxtwo.innerHTML=dayjs().add(2, "day").format('M/D/YY') + "<br/><img src='https://openweathermap.org/img/wn/" + data.list[16].weather[0].icon + "@2x.png'/><br/>Temp: " + data.list[16].main.temp + "°F<br/>Wind: " + data.list[16].wind.speed + " mph<br/>Humidity: " + data.list[16].main.humidity + "%";
   var Boxthree = document.querySelector("#boxthree");
-  Boxthree.innerHTML="Temperature: " + data.list[24].main.temp + " degrees<br/>Windspeed: " + data.list[24].wind.speed + " mph<br/>Humidity: " + data.list[24].main.humidity + "%";
+  Boxthree.innerHTML=dayjs().add(3, "day").format('M/D/YY') + "<br/><img src='https://openweathermap.org/img/wn/" + data.list[24].weather[0].icon + "@2x.png'/><br/>Temp: " + data.list[24].main.temp + "°F<br/>Wind: " + data.list[24].wind.speed + " mph<br/>Humidity: " + data.list[24].main.humidity + "%";
   var Boxfour = document.querySelector("#boxfour");
-  Boxfour.innerHTML="Temperature: " + data.list[32].main.temp + " degrees<br/>Windspeed: " + data.list[32].wind.speed + " mph<br/>Humidity: " + data.list[32].main.humidity + "%";
+  Boxfour.innerHTML=dayjs().add(4, "day").format('M/D/YY') + "<br/><img src='https://openweathermap.org/img/wn/" + data.list[32].weather[0].icon + "@2x.png'/><br/>Temp: " + data.list[32].main.temp + "°F<br/>Wind: " + data.list[32].wind.speed + " mph<br/>Humidity: " + data.list[32].main.humidity + "%";
   var Boxfive = document.querySelector("#boxfive");
-  Boxfive.innerHTML="Temperature: " + data.list[39].main.temp + " degrees<br/>Windspeed: " + data.list[39].wind.speed + " mph<br/>Humidity: " + data.list[39].main.humidity + "%";
+  Boxfive.innerHTML=dayjs().add(5, "day").format('M/D/YY') + "<br/><img src='https://openweathermap.org/img/wn/" + data.list[39].weather[0].icon + "@2x.png'/><br/>Temp: " + data.list[39].main.temp + "°F<br/>Wind: " + data.list[39].wind.speed + " mph<br/>Humidity: " + data.list[39].main.humidity + "%";
 
       localStorage.setItem("cityView", JSON.stringify(data));
      
